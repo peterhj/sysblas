811d8934d57a1f08cc541473c0213caa9e2564a4
Peter Jin <peterhaijin@gmail.com>
2019-06-19 13:11:38 -0700

    Replace bindgen-generated `u32` with `c_int` for enums.

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,20 @@ use std::os::raw::{c_int};
 
 pub const CblasRowMajor: CBLAS_ORDER = 101;
 pub const CblasColMajor: CBLAS_ORDER = 102;
-pub type CBLAS_ORDER = u32;
+pub type CBLAS_ORDER = c_int;
 pub const CblasNoTrans: CBLAS_TRANSPOSE = 111;
 pub const CblasTrans: CBLAS_TRANSPOSE = 112;
 pub const CblasConjTrans: CBLAS_TRANSPOSE = 113;
-pub type CBLAS_TRANSPOSE = u32;
+pub type CBLAS_TRANSPOSE = c_int;
 pub const CblasUpper: CBLAS_UPLO = 121;
 pub const CblasLower: CBLAS_UPLO = 122;
-pub type CBLAS_UPLO = u32;
+pub type CBLAS_UPLO = c_int;
 pub const CblasNonUnit: CBLAS_DIAG = 131;
 pub const CblasUnit: CBLAS_DIAG = 132;
-pub type CBLAS_DIAG = u32;
+pub type CBLAS_DIAG = c_int;
 pub const CblasLeft: CBLAS_SIDE = 141;
 pub const CblasRight: CBLAS_SIDE = 142;
-pub type CBLAS_SIDE = u32;
+pub type CBLAS_SIDE = c_int;
 
 extern "C" {
   pub fn cblas_sasum(
